e122292f41388f8b8358ff0d1f58d6186fc81540
Sam Naish <sam.naish.92@gmail.com>
2020-09-29 20:59:11 +0100

    added help-endpoint to express backend

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res, next) => {
   res.send("hello World!");
 });
 
+app.get("/help", (req, res, next) => {
+  console.log("You need help?");
+  res.send("Hello, do you need help?")
+});
+
 app.listen(8080, () => {
   console.log('Hello world server is running!');
 });
